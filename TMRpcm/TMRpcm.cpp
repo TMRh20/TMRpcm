c974a38592bf4d82fadc24fcf79a048c30c909f2
TMRh20 <tmrh20@gmail.com>
2014-03-01 06:47:56 -0600

    Fix: Fully disable 2nd speaker pin as defined
    Timer pin fully disabled now when #define DISABLE_SPEAKER2 is set

--- TMRpcm/TMRpcm.cpp
+++ TMRpcm/TMRpcm.cpp
@@ -185,7 +185,11 @@ SdFile sFile;
 #if !defined (USE_TIMER2) //NOT using timer2
 void TMRpcm::timerSt(){
 	*ICRn[tt] = resolution;
-	*TCCRnA[tt] = _BV(WGM11) | _BV(COM1A1) | _BV(COM1B0) | _BV(COM1B1); //WGM11,12,13 all set to 1 = fast PWM/w ICR TOP
+	#if !defined (DISABLE_SPEAKER2)
+		*TCCRnA[tt] = _BV(WGM11) | _BV(COM1A1) | _BV(COM1B0) | _BV(COM1B1); //WGM11,12,13 all set to 1 = fast PWM/w ICR TOP
+	#else
+		*TCCRnA[tt] = _BV(WGM11) | _BV(COM1A1); //WGM11,12,13 all set to 1 = fast PWM/w ICR TOP
+	#endif
 	*TCCRnB[tt] = _BV(WGM13) | _BV(WGM12) | _BV(CS10);
   #if defined (MODE2)
   	*ICRn[tt2] = resolution;
